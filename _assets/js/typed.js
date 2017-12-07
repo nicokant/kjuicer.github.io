739165f8b75830b3812a813b40ca3b36b2deb3f5
nicokant <niccolocantu94@hotmail.it>
2017-12-07 18:17:22 +0100

    minify typed.js

---
+++ _assets/js/typed.js
@@ -0,0 +1,11 @@
+import Typed from 'typed.js';
+
+  var options = {
+    strings: typedStrings,
+    typeSpeed: 120,
+    backDelay: 1200,
+    backSpeed: 80,
+    loop: true,
+  }
+
+  new Typed('.typed-rewrite', options)
