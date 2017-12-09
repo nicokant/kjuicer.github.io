a5505e045a9f50952a11614102e52653228c209a
nicokant <niccolocantu94@hotmail.it>
2017-12-09 15:32:14 +0100

    add slick carousel

--- _assets/js/bootstrap.js
+++ _assets/js/bootstrap.js
@@ -1,8 +1,21 @@
 import './globals';
 import Raven from 'raven-js';
 import 'bootstrap';
-
+import 'slick-carousel';
 
 if (Raven) {
   Raven.config('https://fb5b596ad7d24f78ad1708125218cccd@sentry.io/207916').install();
 }
+
+$(document).ready(function() {
+  if ($('#carousel')[0]) {
+    $('#carousel').slick({
+      autoplay: true,
+      fade: true,
+      dots: true,
+      //appendDots: '#carousel-dots',
+      //dotsClass: 'dots',
+    });
+  }
+});
+
