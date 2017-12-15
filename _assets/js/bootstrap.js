ab2e0c293d4df93da1f691fd0f6a96753623ec8a
nicokant <niccolocantu@gmail.com>
2017-12-15 22:01:15 +0100

    sticky header

--- _assets/js/bootstrap.js
+++ _assets/js/bootstrap.js
@@ -2,12 +2,14 @@ import './globals';
 import Raven from 'raven-js';
 import 'bootstrap';
 import 'slick-carousel';
+import 'jquery-sticky';
 
 if (Raven) {
   Raven.config('https://fb5b596ad7d24f78ad1708125218cccd@sentry.io/207916').install();
 }
 
 $(document).ready(function() {
+  $('#top-navigation').sticky({ zIndex: 999999 });
   if ($('#carousel')[0]) {
     $('#carousel').slick({
       autoplay: true,
