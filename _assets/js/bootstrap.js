9137be4bcf5cfeca860db02f1abc1ac0976f45e3
nicokant <niccolocantu@gmail.com>
2017-12-18 19:07:48 +0100

    update header to use headroom

--- _assets/js/bootstrap.js
+++ _assets/js/bootstrap.js
@@ -2,14 +2,20 @@ import './globals';
 import Raven from 'raven-js';
 import 'bootstrap';
 import 'slick-carousel';
-import 'jquery-sticky';
+import Headroom from 'headroom.js';
 
 if (Raven) {
   Raven.config('https://fb5b596ad7d24f78ad1708125218cccd@sentry.io/207916').install();
 }
 
 $(document).ready(function() {
-  $('#top-navigation').sticky({ zIndex: 999999 });
+
+  // configure headroom
+  var headroom  = new Headroom(document.getElementById('top-navigation'), {
+    offset: 75,
+  });
+  headroom.init();
+
   if ($('#carousel')[0]) {
     $('#carousel').slick({
       autoplay: true,
