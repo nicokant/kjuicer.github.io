caa0665ba7d3d2d0b3ce6710225b29716c4c4570
nicokant <niccolocantu@gmail.com>
2017-12-18 19:08:03 +0100

    close menu when it loses focus

--- _assets/js/bootstrap.js
+++ _assets/js/bootstrap.js
@@ -8,11 +8,37 @@ if (Raven) {
   Raven.config('https://fb5b596ad7d24f78ad1708125218cccd@sentry.io/207916').install();
 }
 
+/* close the menu if user clicks elsewhere */
+function dismissMenu(e) {
+  var $btnMenu = $('[data-target="#navbarMenu"]');
+  if (!$btnMenu.is(e.target) && $btnMenu.has(e.target).length === 0 && $('#navbarMenu').has(e.target).length === 0 && $('#navbarMenu').hasClass('show')) {
+    $('#navbarMenu').collapse('hide');
+  }
+}
+
+function debounce(func, wait, immediate) {
+  var timeout;
+  return function() {
+    var context = this, args = arguments;
+    var later = function() {
+      timeout = null;
+      if (!immediate) func.apply(context, args);
+    };
+    var callNow = immediate && !timeout;
+    clearTimeout(timeout);
+    timeout = setTimeout(later, wait);
+    if (callNow) func.apply(context, args);
+  };
+};
+
 $(document).ready(function() {
 
   // configure headroom
   var headroom  = new Headroom(document.getElementById('top-navigation'), {
     offset: 75,
+    onUnpin: function () {
+      $('#navbarMenu').collapse('hide');
+    }
   });
   headroom.init();
 
@@ -26,5 +52,9 @@ $(document).ready(function() {
       //dotsClass: 'dots',
     });
   }
+
+  var debouncedDismiss = debounce(dismissMenu, 250);
+
+  $(document).click(debouncedDismiss);
 });
 
